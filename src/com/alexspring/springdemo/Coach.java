6503ac57bdaac6548c5a6bc73c96fce34f4f12b0
pistachio12 <alexandrustan96@yahoo.ro>
2021-01-29 19:20:05 +0200

    all 3 objects added

--- src/com/alexspring/springdemo/Coach.java
+++
@@ -1,7 +0,0 @@
-package com.alexspring.springdemo;
-
-public interface Coach {
-
-	public String getDailyWorkout();
-	public String getDailyFortune();
-}
--- src/com/alexspring/springdemo/FortuneService.java
+++
@@ -1,7 +0,0 @@
-package com.alexspring.springdemo;
-
-public interface FortuneService {
-	
-	public String getFortune();
-
-}
